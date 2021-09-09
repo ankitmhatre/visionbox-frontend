542202b60400f28e1f27270a70194e07c0c25b5a
Ankit <ankitmhatre.mhatre9@gmail.com>
2021-09-09 12:15:07 +0530

    Vertically centered

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
-import { SectionProps } from '../../utils/SectionProps';
-import ButtonGroup from '../elements/ButtonGroup';
-import Button from '../elements/Button';
-import Image from '../elements/Image';
-import Modal from '../elements/Modal';
-import video_placeholder from './../../assets/images/video-placeholder.jpg'
-import  Particles  from "react-tsparticles";
+import React, { useState } from "react";
+import classNames from "classnames";
+import { SectionProps } from "../../utils/SectionProps";
+import ButtonGroup from "../elements/ButtonGroup";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
+import Modal from "../elements/Modal";
+import video_placeholder from "./../../assets/images/video-placeholder.jpg";
+import Particles from "react-tsparticles";
 const propTypes = {
-  ...SectionProps.types
-}
+  ...SectionProps.types,
+};
 
 const defaultProps = {
-  ...SectionProps.defaults
-}
-
-
-
+  ...SectionProps.defaults,
+};
 
 const Hero = ({
   className,
@@ -28,76 +25,84 @@ const Hero = ({
   invertColor,
   ...props
 }) => {
-
   const [videoModalActive, setVideomodalactive] = useState(false);
 
   const openModal = (e) => {
     e.preventDefault();
     setVideomodalactive(true);
-  }
+  };
 
   const closeModal = (e) => {
     e.preventDefault();
     setVideomodalactive(false);
-  }   
+  };
 
   const outerClasses = classNames(
-    'hero section center-content',
-    topOuterDivider && 'has-top-divider',
-    bottomOuterDivider && 'has-bottom-divider',
-    hasBgColor && 'has-bg-color',
-    invertColor && 'invert-color',
+    "hero section center-content",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
     className
   );
 
   const innerClasses = classNames(
-    'hero-inner section-inner',
-    topDivider && 'has-top-divider',
-    bottomDivider && 'has-bottom-divider'
+    "hero-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
   );
 
-
   return (
-    <section
-      {...props}
-      className={outerClasses}
-        
-    >
+    <section {...props} 
+         className={outerClasses} style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          height: '100vh'
+       }}>
+      <div className={innerClasses}>
      
-        <div className={innerClasses}>
-        <div className="" >
-            <h1 className="mt-0 mb-16 reveal-from-bottom" 
-           // style={{fontWeight :900, fontSize:64, lineHeight:1.19}}
-             data-reveal-delay="200">
-              Providing wings to your <br /><span className="text-color-primary">Vision</span>
-            </h1>
-            <div className="container-xs">
-              <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-              We help companies drive amazing business outcomes by delivering meaningful results using Artificial Intelligence and Machine Learning strategies.</p>
-              <div className="reveal-from-bottom" data-reveal-delay="600">
-                <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile  onClick={()=>{
-                    var rect = document.querySelector('#contact-us').getBoundingClientRect();
-                  
+          <h1
+            className="mt-0 mb-16 reveal-from-bottom"
+            style={{ fontWeight: 900, fontSize: 64, lineHeight: 1.19 }}
+            data-reveal-delay="200"
+          >
+            Providing wings to your <br />
+            <span className="text-color-primary">Vision</span>
+          </h1>
+          <div className="container-xs">
+            <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
+              We help companies drive amazing business outcomes by delivering
+              meaningful results using Artificial Intelligence and Machine
+              Learning strategies.
+            </p>
+            <div className="reveal-from-bottom" data-reveal-delay="600">
+         
+              <ButtonGroup>
+              <buts href="#"   onClick={() => {
+                    var rect = document
+                      .querySelector("#contact-us")
+                      .getBoundingClientRect();
+
                     window.scrollTo({
                       top: rect.top,
                       left: 0,
-                      behavior: 'smooth',
-                      
+                      behavior: "smooth",
                     });
                   }}>
-                    Contact us
-                    </Button>
-                 
-                </ButtonGroup>
-              </div>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+    Contact us</buts>
+                
+              </ButtonGroup>
             </div>
           </div>
         </div>
-
     </section>
   );
-}
+};
 
 Hero.propTypes = propTypes;
 Hero.defaultProps = defaultProps;
--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ import FeaturesTiles from "../components/sections/FeaturesTiles";
 import Testimonial from "../components/sections/Testimonial";
 import Cta from "../components/sections/Cta";
 import Slider from "react-viewport-slider";
-import ArrowDownwardRoundedIcon from '@material-ui/icons/ArrowDownwardRounded';
+import ArrowDownwardRoundedIcon from "@material-ui/icons/ArrowDownwardRounded";
 import ContactUsSection from "../components/sections/ContactUsSection";
 import GenericSection from "../components/sections/GenericSection";
 import Particles from "react-tsparticles";
@@ -16,119 +16,110 @@ const Home = () => {
   }, []);
   return (
     <>
-    
+
       <Slider>
-        <Slider.Item buttonLabel={""}  >
-        <Particles
-        id="tsparticles"
-     style={{
-       zIndex:0
-     }}
-        options={{
-          background: {
-            color: {
-              value: "#000",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {
-            detectsOn: "canvas",
-            events: {
-              onClick: {
-                enable: false,
-                mode: "push",
+        <Slider.Item buttonLabel={""}>
+          <Particles
+            id="tsparticles"
+            style={{
+              zIndex: 0,
+            }}
+            options={{
+              background: {
+                color: {
+                  value: "#000",
+                },
               },
-              onHover: {
-                enable: false,
-                mode: "repulse",
+              fpsLimit: 120,
+              interactivity: {
+                detectsOn: "canvas",
+                events: {
+                  onClick: {
+                    enable: false,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: false,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 500,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 1,
+                  },
+                  repulse: {
+                    distance: 100,
+                    duration: 1,
+                  },
+                },
               },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 500,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
+              particles: {
+                color: {
+                  value: "#ffffff",
+                },
+                links: {
+                  color: "#ffffff",
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 5,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    value_area: 1000,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  random: true,
+                  value: 3,
+                },
               },
-              push: {
-                quantity: 1,
-              },
-              repulse: {
-                distance: 100,
-                duration: 1,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 1000,
-              },
-              value: 80,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 3,
-            },
-          },
-          detectRetina: true,
-        }}
-    >
-     
-      </Particles>
-         
-      <Hero className="illustration-section-01"  />
+              detectRetina: true,
+            }}
+          ></Particles>
           
+          <Hero className="illustration-section-01" />
         </Slider.Item>
-        <div id="services" >
-          <FeaturesTiles
-            
-           
-            className="illustration-section-02"
-            
-          />
+        <div id="services">
+          <FeaturesTiles className="illustration-section-02" />
         </div>
         <div id="testimonials">
           <Testimonial />
         </div>
-        <div id="contact-us" >
+        <div id="contact-us">
           <ContactUsSection />
           <Cta split />
         </div>
-        <div id="about" >
-          <GenericSection >
-          <span>About</span>
+        <div id="about">
+          <GenericSection>
+            <span>About</span>
           </GenericSection>
-       
         </div>
       </Slider>
     </>
