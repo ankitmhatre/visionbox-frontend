b89ecd9bf000bfdfd03326344ea4da4108126e9b
Ankit <ankitmhatre.mhatre9@gmail.com>
2021-09-18 13:42:54 +0530

    Added new routes

---
+++ original_new/src/editableData.js
@@ -0,0 +1,65 @@
+export const allServices = [
+  {
+    image: '../../feature_images/cycle.svg',
+    title: "Full-cycle Development",
+    desc: "From concept to market research to planning to developing, you have got us. We will provide blueprint for your full software cycle development and help you get the best customer experience you need.",
+  },
+  {
+    image: "../../feature_images/technology_consultant.svg",
+    title: "Technology Consulting",
+    desc: "Raising your revenue quotient by finding the best technology for your business to reach your business goals quickly and strategically, we are here for you.",
+  },
+  {
+    image: "../../feature_images/machine_learning.svg",
+    title: "Machine Learning",
+    desc: "We provide Machine Learning services, a leading age technology to solve your complex business data problems and explore a world full of possibilities.",
+  },
+  {
+    image: "../../feature_images/big_data.svg",
+    title: "Big Data/Data Science",
+    desc: "Smarter business moves all by organizing, managing and analysing big data chunks that transform and predict scenarios to deliver best results.",
+  },
+
+  {
+    image: "../../feature_images/data_analytics.svg",
+    title: "Data Analytics",
+    desc: "We will help uncover, discover and extract what's hidden in your data to make right decisions that drive business performance and growth.",
+  },
+  {
+    image: "../../feature_images/computer_vision.svg",
+    title: "Computer Vision",
+    desc: "AI-powered Computer Vision technology to annotate, train and automate digital images and videos. Real time analysis to provide information based on user inputs and choices.",
+  },
+
+  {
+    image: "../../feature_images/nlp.svg",
+    title: "Natural Language Processing",
+    desc: "We will help you build solutions by collecting and annotating text and audio datasets for your projects and deliver faster results.",
+  },
+  {
+    image: "../../feature_images/robotics.svg",
+    title: "Robotics",
+    desc: "An advancing technology to handle your requests efficiently. Automated machine systems to execute specific tasks with little or no human intervention.",
+  },
+];
+
+export const testimonials = [
+  {
+    app: "VP of Engineering at Fatigue Science",
+    name: "David McGrail",
+    review:
+      "The Fatigue Science team had a great experience working with Vishal through the ScaleAI program. He helped our team realize the power of our existing data and come up with a plan to implement a machine learning solution that materially improved the sleep and fatigue forecasts of thousands of high risk individuals (mining, oil&gas, transportation) across the world. Our project included both theoretical exploration of models, as well as applying the models and infrastructure to make live predictions for our customers. We fully expect to continue working with Vishal in the future after the success of our initial projects.",
+  },
+  {
+    app: "AppName",
+    name: "Person Name",
+    review:
+      "adsadaad",
+  },
+  {
+    app: "AppName",
+    name: "Person Name",
+    review:
+      "adadauehnai",
+  },
+];
